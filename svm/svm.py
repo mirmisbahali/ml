9435ed0a9c0667ade6e73cbe3c02069b799b71a9
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-09 19:11:32 +0530

    svm

--- svm/svm.py
+++ svm/svm.py
@@ -1,6 +1,8 @@
 import sklearn
 from sklearn import datasets
 from sklearn import svm
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
 
 cancer = datasets.load_breast_cancer()
 
@@ -12,7 +14,17 @@ y = cancer.target
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 
-print(x_train, y_train)
+# print(x_train, y_train)
 classes = ['malignant', 'benign']
+
+clf = svm.SVC(kernel="linear")
+# clf = KNeighborsClassifier(n_neighbors=9)
+clf.fit(x_train, y_train)
+
+y_pred = clf.predict(x_test)
+
+acc = metrics.accuracy_score(y_test, y_pred)
+
+print(acc)
 
 
