933495073c0910e0f829fdd82979e600392be592
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-02 21:16:58 +0530

    lemmatizing words

--- nltk/chatbot/training.py
+++ nltk/chatbot/training.py
@@ -11,7 +11,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 
@@ -23,10 +23,13 @@ ignore_letters = ['?', '!', '.', ',']
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern)
-        words.append(word_list)
+        words.extend(word_list)
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
 
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+print(words)
